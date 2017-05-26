5be83874e46ec31bb13c69bbfdef430882b339ab
hanhthaoluu <hanhthaoluu@gmail.com>
2017-05-26 12:16:12 -0700

    now after each user's response there is an alert

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ alert('Welcome to Thao\'s Page!');
 var user = prompt('What is your name?');
 alert('Welcome ' + user + '!');
 var confirmed = confirm(user + ', are you ready to play a guessing game?');
-if (confirmed === true){
+if(confirmed === true){
   alert('Ok, let\'s play!');
   console.log('The user is ready to play!')
 } else {
@@ -11,17 +11,19 @@ if (confirmed === true){
 }
 var familyQuestion = prompt('Does Thao have kids?').toLowerCase();
 if (familyQuestion === 'yes'){
-  console.log('Excellent! You are right!');
+  alert('Excellent! You are right!');
+  console.log('The user got the familyQuestion right.')
 } else {
-  alert('Incorrect! Guess again!');
+  alert('Incorrect!');
   console.log('The user got it wrong...but let\'s move forward anyway.');
 }
 console.log('Thao has two beautiful kids, a five years old daughter and');
 console.log('a 9 months old son. Besides spending time learning to code and');
 console.log('having fun with two kids, Thao has other hobbies.');
 var hobbyQuestion = prompt('Is gardening Thao\'s favorite hobby?').toUpperCase();
-if (hobbyQuestion === 'Yes'){
-  console.log('Correct! You\'re good at this game!');
+if(hobbyQuestion === 'YES'){
+  alert('Correct! You\'re good at this game!');
+  console.log(user + ' is a great player!');
 } else {
   prompt('Incorrect! Try again! Guess it right next time!');
 }
@@ -29,8 +31,9 @@ console.log('Summer is almost here. Are you excited?! During the warm months,');
 console.log('Thao likes to do something else also besides spending time');
 console.log('in the garden.');
 var hobbyQuestion2 = prompt('Has Thao been to the Zion National Park?').toUpperCase();
-if (hobbyQuestion2 === 'Yes'){
-  console.log('You are a genius! Let\'s move forward!');
+if(hobbyQuestion2 === 'YES'){
+  alert('You are a genius! Let\'s move forward!');
+  console.log(user + ' is really good at this guessing game!');
 } else {
   alert('Incorrect! But don\'t give up. You will get it right next time!');
   console.log('The user answered incorrectly, but it\'s ok.');
@@ -39,15 +42,17 @@ console.log('Thao loves to take roadtrips. One time, Thao drove her family');
 console.log('back to Seattle from California nonstop.');
 var hobbyQuestion3 = prompt('Do you think Thao got a speeding ticket on her way back from California?').toLowerCase();
 if (hobbyQuestion3 === 'yes'){
-  console.log('Awesome! You got it right!');
+  alert('Awesome! You got it right!');
+  console.log(user + ' got the hobbyQuestion3 correct!')
 } else {
-  console.log('Incorrect!');
+  alert('Incorrect! Next question...');
+  console.log(user + ' got the hobbyQuestion3 wrong.');
 }
-var lifeValueQuestion= prompt('Is Thao a vegetarian?').toLowerCase();
-if (lifeValueQuestion === 'yes'){
-  alert('Fanstastic! You got it right! Thao loves plants but she eats mainly plants. This is a conflict of interest that Thao cannot resolve.');
+var lifeValueQuestion = prompt('Is Thao a vegetarian?').toLowerCase();
+if(lifeValueQuestion === 'yes'){
+  alert('Fanstastic! You got it right!');
   console.log(user + ' got the question right.');
 } else {
   alert('Sorry, you got it wrong!');
-  console.log(user + ' got the question wrong.');
+  console.log(user + ' got the lifeValueQuestion wrong.');
 }
