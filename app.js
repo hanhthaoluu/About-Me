4673bdd63556cbc6c6ab7fa44984efe493366d6e
hanhthaoluu <hanhthaoluu@gmail.com>
2017-05-27 22:38:59 -0700

    successfully accommdating users entering y/n

--- app.js
+++ app.js
@@ -5,54 +5,66 @@ alert('Welcome ' + user + '!');
 var confirmed = confirm(user + ', are you ready to play a guessing game?');
 if(confirmed === true){
   alert('Ok, let\'s play!');
-  console.log('The user is ready to play!')
+  console.log('The user is ready to play!');
 } else {
   console.log('The user is not ready to play...but let\'s proceed anyway.');
 }
 var familyQuestion = prompt('Does Thao have kids?').toLowerCase();
-if (familyQuestion === 'yes'){
+if (familyQuestion === 'yes' || familyQuestion === 'y'){
   alert('Excellent! You are right!');
-  console.log('The user got the familyQuestion right.')
-} else {
+  console.log(user + ' got the familyQuestion question right.');
+} else if (familyQuestion === 'no' || familyQuestion === 'n'){
   alert('Incorrect!');
-  console.log('The user got it wrong...but let\'s move forward anyway.');
+  console.log(user + ' got the familyQuestion question wrong.');
+} else {
+  alert('Invalid response.');
 }
 console.log('Thao has two beautiful kids, a five years old daughter and');
 console.log('a 9 months old son. Besides spending time learning to code and');
 console.log('having fun with two kids, Thao has other hobbies.');
 var hobbyQuestion = prompt('Is gardening Thao\'s favorite hobby?').toUpperCase();
-if(hobbyQuestion === 'YES'){
-  alert('Correct! You\'re good at this game!');
+if(hobbyQuestion === 'YES' || hobbyQuestion === 'Y'){
+  alert('Superstar! You\'re good at this game!');
   console.log(user + ' is a great player!');
+} else if (hobbyQuestion === 'NO' || hobbyQuestion === 'N'){
+  alert('Incorrect! Sorry!');
+  console.log(user + 'got the hobbyQuestion question wrong.')
 } else {
-  prompt('Incorrect! Try again! Guess it right next time!');
+  alert('Invalid response.');
 }
 console.log('Summer is almost here. Are you excited?! During the warm months,');
 console.log('Thao likes to do something else also besides spending time');
 console.log('in the garden.');
 var hobbyQuestion2 = prompt('Has Thao been to the Zion National Park?').toUpperCase();
-if(hobbyQuestion2 === 'YES'){
+if(hobbyQuestion2 === 'YES' || hobbyQuestion2 === 'Y'){
   alert('You are a genius! Let\'s move forward!');
   console.log(user + ' is really good at this guessing game!');
-} else {
+} else if (hobbyQuestion2 === 'NO' || hobbyQuestion2 === 'N'){
   alert('Incorrect! But don\'t give up. You will get it right next time!');
   console.log('The user answered incorrectly, but it\'s ok.');
+} else {
+  alert('Invalid response.');
 }
 console.log('Thao loves to take roadtrips. One time, Thao drove her family');
 console.log('back to Seattle from California nonstop.');
 var hobbyQuestion3 = prompt('Do you think Thao got a speeding ticket on her way back from California?').toLowerCase();
-if (hobbyQuestion3 === 'yes'){
+if (hobbyQuestion3 === 'yes' || hobbyQuestion3 === 'y'){
   alert('Awesome! You got it right!');
-  console.log(user + ' got the hobbyQuestion3 correct!')
-} else {
+  console.log(user + ' got the hobbyQuestion3 question correct!')
+} else if (hobbyQuestion3 === 'no' || hobbyQuestion3 === 'n'){
   alert('Incorrect! Next question...');
-  console.log(user + ' got the hobbyQuestion3 wrong.');
+  console.log(user + ' got the hobbyQuestion3 question wrong.');
+} else {
+  alert('Invalid response.');
 }
 var lifeValueQuestion = prompt('Is Thao a vegetarian?').toLowerCase();
-if(lifeValueQuestion === 'yes'){
-  alert('Fanstastic! You got it right!');
-  console.log(user + ' got the question right.');
-} else {
+if(lifeValueQuestion === 'yes' || lifeValueQuestion === 'y'){
+  alert('Fanstastic! You are really good at this game!');
+  console.log(user + ' got the lifeValueQuestion question right.');
+} else if (lifeValueQuestion === 'no' || lifeValueQuestion === 'n'){
   alert('Sorry, you got it wrong!');
-  console.log(user + ' got the lifeValueQuestion wrong.');
+  console.log(user + ' got the lifeValueQuestion question wrong.');
+} else {
+  alert('Invalid response.');
 }
+alert('This is the end of the guessing game. Hope you enjoyed playing the game. Thank you for playing!')
