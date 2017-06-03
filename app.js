b7ba6c6fdb62f819bd912bbb322ba0794169066e
hanhthaoluu <hanhthaoluu@gmail.com>
2017-06-02 23:28:06 -0700

    successfully tally only the correct ones and fixed the STATES question for when there is no match

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 alert('Hello there! Welcome to Thao\'s Page!');
-var user = prompt('What is your name?');
+var user = prompt('What is your name?').toUpperCase();
 alert('Welcome ' + user + '!');
 
-var confirmed = confirm(user + ', are you ready to play a guessing game?');
+var confirmed = confirm(user + ', are you ready to play a guessing game? Unless instructed otherwise, enter either "Yes" or "Y" or "No" or "N". Not case sensitive.');
 
 if(confirmed === true){
   alert('Ok, let\'s play!');
@@ -12,8 +12,7 @@ if(confirmed === true){
   console.log('The user is not ready to play...but let\'s proceed anyway.');
 }
 var correct = 0;
-var incorrect = 0;
-
+//var totalScore = correct/7;
 
 var familyQuestion = 'Does Thao have kids?';
 var answerFamilyQuestion = prompt(familyQuestion).toUpperCase();
@@ -24,7 +23,7 @@ if (answerFamilyQuestion === 'YES' || answerFamilyQuestion === 'Y'){
   console.log(user + ' got the familyQuestion question right.');
 } else if (answerFamilyQuestion === 'NO' || answerFamilyQuestion === 'N'){
   alert('Incorrect! Don\'t worry. You will get it right next time.');
-  incorrect += 1;
+
   console.log(user + ' got the familyQuestion question wrong.');
 } else {
   alert('Invalid response.');
@@ -41,7 +40,6 @@ if(answerHobbyQuestion === 'YES' || answerHobbyQuestion === 'Y'){
   console.log(user + ' is a great player!');
 } else if (answerHobbyQuestion === 'NO' || answerHobbyQuestion === 'N'){
   alert('Incorrect! Keep going! You will get it right next time.');
-  incorrect += 1;
   console.log(user + 'got the hobbyQuestion question wrong.');
 } else {
   alert('Invalid response.');
@@ -59,7 +57,6 @@ if(answerHobbyQuestion2 === 'YES' || answerHobbyQuestion2 === 'Y'){
   console.log(user + ' is really good at this guessing game!');
 } else if (answerHobbyQuestion2 === 'NO' || answerHobbyQuestion2 === 'N'){
   alert('Incorrect! But don\'t give up. You will get it right next time!');
-  incorrect += 1;
   console.log('The user answered incorrectly, but it\'s ok.');
 } else {
   alert('Invalid response.');
@@ -74,7 +71,6 @@ if (answerHobbyQuestion3 === 'YES' || answerHobbyQuestion3 === 'Y'){
   console.log(user + ' got the hobbyQuestion3 question correct!');
 } else if (answerHobbyQuestion3 === 'NO' || answerHobbyQuestion3 === 'N'){
   alert('Incorrect! Keep going and you will get the next question right.');
-  incorrect += 1;
   console.log(user + ' got the hobbyQuestion3 question wrong.');
 } else {
   alert('Invalid response.');
@@ -89,7 +85,6 @@ if(answerLifeValueQuestion === 'YES' || answerLifeValueQuestion === 'Y'){
   console.log(user + ' got the lifeValueQuestion question right.');
 } else if (answerLifeValueQuestion === 'NO' || answerLifeValueQuestion === 'N'){
   alert('Sorry, you got it wrong!');
-  incorrect += 1;
   console.log(user + ' got the lifeValueQuestion question wrong.');
 } else {
   alert('Invalid response.');
@@ -122,52 +117,60 @@ while (guessingNumberFloweringPots < 4) {
 alert('Thao is plantholic. Thao has 45 pots in her house, not counting the outdoor pots.');
 
 //This works
-var states = ['Virginia', 'Florida', 'Georgia', 'Oregon', 'Ohio', 'Pennsylvania', 'California', 'Maryland', 'Texas', 'Utah'];
+var states = [' Virginia', ' Florida', ' Georgia', ' Oregon', ' Ohio', ' Pennsylvania', ' California', ' Maryland', ' Texas', ' Utah'];
 var myStates = ['FLORIDA', 'PENNSYLVANIA'];
 var userSixAnswerStatesILived = [];
 
-guessingStatesILived = 0;
+var guessingStatesILived = 0;
 while(guessingStatesILived < 6) {
-  var userAnswerStatesILived = prompt("Besides Washington state, what other two states have I lived in the past?" + "Choose from these possible correct states: " + states + ".").toUpperCase();
+  var userAnswerStatesILived = prompt("Besides Washington state, what other two states have I lived in the past?" + " Choose from these possible correct states: " + states + "? Enter 1 state for each prompt or box. You have 6 chances to get a single correct answer.").toUpperCase();
   userSixAnswerStatesILived.push(userAnswerStatesILived);
   guessingStatesILived ++;
 }
 
 for(var i = 0; i < myStates.length; i++) {
-  document.write(myStates[i]);
+  console.log(myStates[i]);
   for (var j = 0; j < userSixAnswerStatesILived.length; j++) {
-    document.write(userSixAnswerStatesILived[j]);
+    console.log(userSixAnswerStatesILived[j]);
 
     if (myStates[i] === userSixAnswerStatesILived[j]) {
       alert('Your answer match!');
-      document.write('Match: ' + myStates[i]);
+      //console.log('Match: ' + myStates[i]);
       correct ++;
+      //guessingStatesILived = 6;
+      console.log('Match: ' + myStates[i]);
+     }
+    //else {
+      //alert('No match! All your six guesses were wrong. Better luck next time!');
+      //incorrect ++;
+      //guessingStatesILived = 6;
+
     }
   }
-}
 
 
 
-  if ((userAnswerStatesILived === "PENNSYLVANIA" && userAnswerStatesILived === "FLORIDA") {
-    alert(Fanstastic! You got it right! You are a psychic!);
-    guessingStatesILived = 6;
-  } else if (userAnswerStatesILived ){
-
-  }
-}
 
 
 alert('This is the end of the guessing game. Hope you enjoyed playing the game. Thank you for playing!');
 
+var totalScore = (correct + '/7');
+
 document.write("THE GUESSING GAME" + "<br>");
-document.write("Question 1: Does Thao have kids?" + "<br>" + "Your answer: " + answerFamilyQuestion + "<br>");
-document.write("Question 2: Is gardening Thao\'s favorite hobby?" + "<br>" + "Your answer: " + answerHobbyQuestion + "<br>");
-document.write("Question 3: Has Thao been to the Zion National Park and Bryce Kenyon National Park?" + "<br>" + "Your answer: " + answerHobbyQuestion2 + "<br>");
-document.write("Question 4: Does Thao like to take roadtrips? Has Thao ever driven her family back to Seattle from California nonstop?" + "<br>" + "Your answer: " + answerHobbyQuestion3 + "<br>");
-document.write("Question 5: Is Thao a vegetarian?" + "<br>" + "Your answer: " + answerLifeValueQuestion + "<br>");
-document.write("RESULTS: " + user + "You got " + correct + " questions correct and " + incorrect + " questions incorrect.");
-document.write(floweringPotsNumberQuestion + "<br>");
-document.write("The correct number is 43!");
+document.write("QUESTION 1: Does Thao have kids?" + "<br>" + "YOUR ANSWER: " + answerFamilyQuestion + "<br>");
+document.write("QUESTION 2: Is gardening Thao\'s favorite hobby?" + "<br>" + "YOUR ANSWER: " + answerHobbyQuestion + "<br>");
+document.write("QUESTION 3: Has Thao been to the Zion National Park and Bryce Kenyon National Park?" + "<br>" + "YOUR ANSWER: " + answerHobbyQuestion2 + "<br>");
+document.write("QUESTION 4: Does Thao like to take roadtrips? Has Thao ever driven her family back to Seattle from California nonstop?" + "<br>" + "YOUR ANSWER: " + answerHobbyQuestion3 + "<br>");
+document.write("QUESTION 5: Is Thao a vegetarian?" + "<br>" + "YOUR ANSWER: " + answerLifeValueQuestion + "<br>");
+
+
+document.write("QUESTION 6: How many planting pots are there in Thao\'s house?" + "<br>" + "YOUR ANSWER: " + userAnswerFloweringPotsNumber + "<br>");
+
+document.write("QUESTION 7: Besides Washington state, what other two states have I lived in the past? Choose from these possible correct states: " + states + "?" + "<br>" + "YOUR ANSWER: " + userSixAnswerStatesILived + "<br>");
+
+document.write("RESULTS: " + user + ", you got " + correct + " questions correct." + "<br>" + "YOUR SCORE: " + totalScore);
+
+
 /*var guessingGameQuestions = [familyQuestion, answerFamilyQuestion, hobbyQuestion, hobbyQuestion2, hobbyQuestion3, lifeValueQuestion];
 for(var i = 0; i < guessingGameQuestions.length; i++) {
   document.write(guessingGameQuestions[i]);
